15356eb616f10b7d3e8825668edd5f8d9ff934d7
Ramneek Singh <ramneek2097.cse18@chitkara.edu.in>
2019-06-23 18:47:35 +0530

    Update progress.js

--- progress.js
+++ progress.js
@@ -1,12 +1,17 @@
 
 function myfunction(x){
-var m=document.getElementById("m").value;
-var n=document.getElementById("n").value;
-var o=(m*1024/n);
-document.getElementById("p1").innerHTML=o;
-if(x>100){}else{
-document.getElementById("s").style.width=x+"%";
-document.getElementById("s").innerHTML=x+"%";
-x=x+1;
-setTimeout('myfunction('+x+')',o*10);}
-}
+    var m=document.getElementById("m").value;            //getting downloading size in mb 
+    var n=document.getElementById("n").value;            //getting downloading speed at that time
+    var o=(m*1024/n);                                    //calculating downloading time
+    document.getElementById("p1").innerHTML=o;
+    if(x>100)
+    {
+    }
+    else
+    {
+    document.getElementById("s").style.width=x+"%";       //progressing width
+    document.getElementById("s").innerHTML=x+"%";         //progressing %
+    x=x+1;
+    setTimeout('myfunction('+x+')',o*10);}                //time function
+    }
+    
